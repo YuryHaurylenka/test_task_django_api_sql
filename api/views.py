4775a615c2da575840ce63a6c88414e5b4144c24
GavrilenkoYura <gshshaaha@gmail.com>
2024-11-11 02:40:28 +0300

    create LinkViewSet

--- api/views.py
+++ api/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework import permissions, viewsets
 
-# Create your views here.
+from .models import Link
+from .serializers import LinkSerializer
+
+
+class LinkViewSet(viewsets.ModelViewSet):
+    queryset = Link.objects.all()
+    serializer_class = LinkSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
