12483cfda86eae7b31a797761e83b19f91de9078
GavrilenkoYura <gshshaaha@gmail.com>
2024-11-11 03:48:01 +0300

    add Collection serializer

--- api/serializers.py
+++ api/serializers.py
@@ -1,10 +1,28 @@
 from rest_framework import serializers
 
-from .models import Link
+from .models import Collection, Link
 
 
 class LinkSerializer(serializers.ModelSerializer):
     class Meta:
         model = Link
-        fields = ['id', 'user', 'url', 'title', 'description', 'created_at']
-        read_only_fields = ['user', 'created_at']
+        fields = ["id", "url", "title", "description", "image", "type", "created_at"]
+
+
+class CollectionSerializer(serializers.ModelSerializer):
+    links = LinkSerializer(many=True, read_only=True)
+    link_ids = serializers.PrimaryKeyRelatedField(
+        queryset=Link.objects.all(), write_only=True, many=True, source="links"
+    )
+
+    class Meta:
+        model = Collection
+        fields = [
+            "id",
+            "title",
+            "description",
+            "links",
+            "link_ids",
+            "created_at",
+            "updated_at",
+        ]
