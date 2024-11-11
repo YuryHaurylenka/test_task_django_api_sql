0edb16ad9087cf38cf5e2a2bbc6edb7f975a76ff
GavrilenkoYura <gshshaaha@gmail.com>
2024-11-11 16:05:05 +0300

    update LinkViewSet and CollectionViewSet with improved data handling and session-based token storage

--- api/views.py
+++ api/views.py
@@ -1,3 +1,4 @@
+from rest_framework import status
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import filters
 from rest_framework import permissions, viewsets
@@ -5,39 +6,32 @@ from rest_framework import status
 from rest_framework_simplejwt.views import TokenObtainPairView
 
 from .models import Collection, Link
+from .permissions import IsOwnerOrReadOnly
 from .serializers import CollectionSerializer, LinkSerializer
-from .utils import fetch_og_data
 
 
 class LinkViewSet(viewsets.ModelViewSet):
     queryset = Link.objects.all()
     serializer_class = LinkSerializer
-    permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [permissions.IsAuthenticated, IsOwnerOrReadOnly]
     filter_backends = [DjangoFilterBackend, filters.SearchFilter]
-    filterset_fields = ["user"]
+    filterset_fields = ["type"]
     search_fields = ["title", "description"]
 
-    def perform_create(self, serializer):
-        url = self.request.data.get("url")
-        og_data = fetch_og_data(url)
+    def get_queryset(self):
+        return Link.objects.filter(user=self.request.user)
 
-        if og_data:
-            serializer.save(
-                user=self.request.user,
-                url=url,
-                title=og_data.get("title"),
-                description=og_data.get("description"),
-                image=og_data.get("image"),
-                type=og_data.get("type"),
-            )
-        else:
-            serializer.save(user=self.request.user)
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
 
 
 class CollectionViewSet(viewsets.ModelViewSet):
     queryset = Collection.objects.all()
     serializer_class = CollectionSerializer
-    permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [permissions.IsAuthenticated, IsOwnerOrReadOnly]
+
+    def get_queryset(self):
+        return Collection.objects.filter(user=self.request.user)
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
