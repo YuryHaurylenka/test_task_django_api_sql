0d2b56e00f1686129876d6a31d1e11fb73f58348
GavrilenkoYura <gshshaaha@gmail.com>
2024-11-11 03:46:44 +0300

    add open graph data save to LinkViewSet

--- api/views.py
+++ api/views.py
@@ -4,6 +4,7 @@ from rest_framework import permissions, viewsets
 
 from .models import Link
 from .serializers import LinkSerializer
+from .utils import fetch_og_data
 
 
 class LinkViewSet(viewsets.ModelViewSet):
@@ -15,4 +16,17 @@ class LinkViewSet(viewsets.ModelViewSet):
     search_fields = ["title", "description"]
 
     def perform_create(self, serializer):
-        serializer.save(user=self.request.user)
+        url = self.request.data.get("url")
+        og_data = fetch_og_data(url)
+
+        if og_data:
+            serializer.save(
+                user=self.request.user,
+                url=url,
+                title=og_data.get("title"),
+                description=og_data.get("description"),
+                image=og_data.get("image"),
+                type=og_data.get("type"),
+            )
+        else:
+            serializer.save(user=self.request.user)
