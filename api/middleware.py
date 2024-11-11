6861a4919cf386f52a2501a7ae39f04608a08a4e
GavrilenkoYura <gshshaaha@gmail.com>
2024-11-12 01:28:56 +0300

    add public endpoints to not require token

--- api/middleware.py
+++ api/middleware.py
@@ -1,8 +1,25 @@
 from django.utils.deprecation import MiddlewareMixin
+from django.urls import resolve
 
 
 class AddAuthorizationHeaderMiddleware(MiddlewareMixin):
+    PUBLIC_ENDPOINTS = [
+        "/auth/register/",
+        "/auth/users/reset_password/",
+        "/auth/users/reset_password_confirm/",
+        "/auth/login/",
+        "/auth/jwt/refresh/",
+        "/auth/jwt/verify/",
+        "/swagger/",
+        "/admin",
+    ]
+
     def process_request(self, request):
-        access_token = request.session.get("access_token")
-        if access_token and "HTTP_AUTHORIZATION" not in request.META:
-            request.META["HTTP_AUTHORIZATION"] = f"Bearer {access_token}"
+        if any(request.path.startswith(endpoint) for endpoint in self.PUBLIC_ENDPOINTS):
+            return
+
+        auth_header = request.headers.get("Authorization")
+        if not auth_header:
+            from rest_framework.exceptions import AuthenticationFailed
+
+            raise AuthenticationFailed("Token is missing")
