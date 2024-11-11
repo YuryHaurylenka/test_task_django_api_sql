d53ce61de16e8117e4860b250e7692302fe6ae96
GavrilenkoYura <gshshaaha@gmail.com>
2024-11-11 03:03:42 +0300

    add filters and search fields to LinkViewSet

--- api/views.py
+++ api/views.py
@@ -1,3 +1,5 @@
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import filters
 from rest_framework import permissions, viewsets
 
 from .models import Link
@@ -8,6 +10,9 @@ class LinkViewSet(viewsets.ModelViewSet):
     queryset = Link.objects.all()
     serializer_class = LinkSerializer
     permission_classes = [permissions.IsAuthenticated]
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter]
+    filterset_fields = ["user"]
+    search_fields = ["title", "description"]
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
