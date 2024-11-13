588fea0ed111e47f6ef9c80737981dff032fdc42
GavrilenkoYura <gshshaaha@gmail.com>
2024-11-13 13:24:11 +0300

    add func save_to_csv to save sql query, change return of error

--- api/utils.py
+++ api/utils.py
@@ -1,3 +1,4 @@
+import csv
 import logging
 import re
 
@@ -12,6 +13,7 @@ def fetch_og_data(url):
         response = requests.get(url, timeout=5)
         response.raise_for_status()
         soup = BeautifulSoup(response.content, "html.parser")
+
         og_data = {
             "title": (
                 soup.find("meta", property="og:title")["content"]
@@ -31,33 +33,58 @@ def fetch_og_data(url):
             "type": (
                 soup.find("meta", property="og:type")["content"]
                 if soup.find("meta", property="og:type")
-                else "website"
+                else None
             ),
         }
 
         if not og_data["title"]:
             og_data["title"] = soup.title.string if soup.title else "No title available"
+
         if not og_data["description"]:
             og_data["description"] = (
                 soup.find("meta", attrs={"name": "description"})["content"]
                 if soup.find("meta", attrs={"name": "description"})
                 else "No description available"
             )
+
         if not og_data["image"]:
             og_data["image"] = "https://example.com/default-image.jpg"
+
+        if not og_data["type"]:
+            og_data["type"] = "website"
 
         return og_data
 
     except Exception as e:
         logger.error(f"Failed to fetch Open Graph data for {url}: {e}")
         return {
-            "title": "No title available",
-            "description": "No description available",
-            "image": "https://example.com/default-image.jpg",
-            "type": "website",
+            "title": "Failed to fetch Open Graph data",
+            "description": "Failed to fetch Open Graph data",
+            "image": "Failed to fetch Open Graph data",
+            "type": "error",
         }
 
 
 def extract_uri(url):
     url = re.sub(r"^https?://(www\.)?", "", url, flags=re.IGNORECASE)
     return url.split("/", 1)[-1] if "/" in url else url
+
+
+def save_to_csv(data, file_path):
+    with open(file_path, "w", newline="", encoding="utf-8") as csvfile:
+        fieldnames = [
+            "email",
+            "count_links",
+            "website",
+            "book",
+            "article",
+            "music",
+            "video",
+            "company",
+            "object",
+            "error",
+        ]
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        writer.writeheader()
+        for row in data:
+            writer.writerow(row)
