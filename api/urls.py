e54f15469bb9935deab90b68b95cafa3dd6c03b7
GavrilenkoYura <gshshaaha@gmail.com>
2024-11-13 13:25:03 +0300

    add top_users.sql query

--- api/urls.py
+++ api/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import LinkViewSet, CollectionViewSet
+from .views import LinkViewSet, CollectionViewSet, CustomTopUsersViewSet
 
 router = DefaultRouter()
 router.register("links", LinkViewSet, basename="link")
 router.register("collections", CollectionViewSet, basename="collection")
+router.register("users", CustomTopUsersViewSet, basename="top-users")
 
 urlpatterns = [
     path("", include(router.urls)),
---
+++ top_users.sql
@@ -0,0 +1,38 @@
+WITH link_counts AS (
+    SELECT
+        u.email,
+        u.date_joined,
+        COUNT(l.id) AS count_links,
+        SUM(CASE WHEN l.type = 'website' THEN 1 ELSE 0 END) AS website,
+        SUM(CASE WHEN l.type = 'books.book' THEN 1 ELSE 0 END) AS book,
+        SUM(CASE WHEN l.type = 'article' THEN 1 ELSE 0 END) AS article,
+        SUM(CASE WHEN l.type = 'music' THEN 1 ELSE 0 END) AS music,
+        SUM(CASE WHEN l.type = 'video.other' THEN 1 ELSE 0 END) AS video,
+        SUM(CASE WHEN l.type = 'company' THEN 1 ELSE 0 END) AS company,
+        SUM(CASE WHEN l.type = 'object' THEN 1 ELSE 0 END) AS object,
+        SUM(CASE WHEN l.type = 'error' THEN 1 ELSE 0 END) AS error
+    FROM
+        api_customuser AS u
+    LEFT JOIN
+        api_link AS l ON u.id = l.user_id
+    GROUP BY
+        u.id, u.email, u.date_joined
+)
+SELECT
+    email,
+    count_links,
+    website,
+    book,
+    article,
+    music,
+    video,
+    company,
+    object,
+    error,
+    date_joined
+FROM
+    link_counts
+ORDER BY
+    count_links DESC,
+    date_joined ASC
+LIMIT 10;
