ec364ec1ff99e5e4bd9a7da1a547ae366568cb33
GavrilenkoYura <gshshaaha@gmail.com>
2024-11-13 13:25:14 +0300

    add top_users view to execute top_users.sql file

--- api/views/user.py
+++ api/views/user.py
@@ -1,9 +1,11 @@
+import os
+
+from django.db import connection
 from djoser.views import UserViewSet
 from drf_yasg import openapi
 from drf_yasg.utils import swagger_auto_schema
-from rest_framework import permissions, viewsets
-from rest_framework import status
-from rest_framework.exceptions import ValidationError
+from rest_framework import status, viewsets
+from rest_framework.decorators import action
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework_simplejwt.views import (
@@ -12,14 +14,7 @@ from rest_framework_simplejwt.views import (
     TokenVerifyView,
 )
 
-from ..models import Collection, Link
-from ..permissions import IsOwnerOrReadOnly
-from ..serializers import (
-    CollectionDetailSerializer,
-    LinkCreateSerializer,
-    LinkDetailSerializer,
-)
-from ..utils import fetch_og_data
+from api.utils import save_to_csv
 
 
 class CustomTokenObtainPairView(TokenObtainPairView):
@@ -121,3 +116,71 @@ class CustomTokenVerifyView(TokenVerifyView):
     )
     def post(self, request, *args, **kwargs):
         return super().post(request, *args, **kwargs)
+
+
+class CustomTopUsersViewSet(viewsets.ViewSet):
+    @action(
+        detail=False,
+        methods=["get"],
+        url_path="top-users",
+    )
+    @swagger_auto_schema(
+        operation_summary="Top 10 Users",
+        operation_description="Retrieve top 10 users with the highest number of saved links, sorted by date of registration.",
+        responses={
+            200: openapi.Response(
+                description="Top 10 users retrieved successfully",
+                examples={
+                    "application/json": [
+                        {
+                            "email": "user1@example.com",
+                            "count_links": 99,
+                            "date_joined": "2024-01-12T10:30:45",
+                        },
+                        {
+                            "email": "user2@example.com",
+                            "count_links": 95,
+                            "date_joined": "2024-02-03T14:22:10",
+                        },
+                    ]
+                },
+            ),
+            404: "SQL file not found",
+        },
+    )
+    def top_users(self, request):
+        base_dir = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
+        sql_file_path = os.path.join(base_dir, "top_users.sql")
+
+        if not os.path.exists(sql_file_path):
+            return Response(
+                {"error": "SQL file not found"}, status=status.HTTP_404_NOT_FOUND
+            )
+
+        with open(sql_file_path, "r") as file:
+            query = file.read()
+
+        with connection.cursor() as cursor:
+            cursor.execute(query)
+            results = cursor.fetchall()
+
+        users_data = [
+            {
+                "email": row[0],
+                "count_links": row[1],
+                "website": row[2],
+                "book": row[3],
+                "article": row[4],
+                "music": row[5],
+                "video": row[6],
+                "company": row[7],
+                "object": row[8],
+                "error": row[9],
+            }
+            for row in results
+        ]
+
+        output_file_path = os.path.join(base_dir, "top_users_output.csv")
+        save_to_csv(users_data, output_file_path)
+
+        return Response(users_data, status=status.HTTP_200_OK)
